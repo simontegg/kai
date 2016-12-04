fe65ba36d210ace70eec27814a391cc281e2aa50
simontegg <simon@enspiral.com>
2016-12-04 18:37:41 +1300

    prod-cofig

---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :kai, Kai.Endpoint,
+  secret_key_base: "TeNlDG2s3pRduGEa15RaD2/G0iZz9nCpZlRUb2vRhxWmxwqGCiF5JBJTe5oDuBR1"
+
+# Configure your database
+config :kai, Kai.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "kai_prod",
+  pool_size: 20
