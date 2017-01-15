d9c090a65119e2be77c4e5b495c146c4d8244696
simontegg <simon@enspiral.com>
2017-01-15 14:49:48 +1300

    ip

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :kai, Kai.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "nameless-savannah-55523.herokuapp.com", port: 443],
+  url: [ip: {23, 92, 53, 38} port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
